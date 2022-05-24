9ab9bf8168d4d83c4c95e16d3def6ab0f4f35a48
BATU1579 <batu1234567890@outlook.com>
2022-05-25 03:59:12 +0800

    ✨ feat: 转为使用TypeScript编写脚本
    使用 ts 声明文件来实现代码补全和类型检查

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author: BATU1579
+ * @CreateDate: 2022-05-24 16:58:03
+ * @LastEditor: BATU1579
+ * @LastTime: 2022-05-25 03:37:06
+ * @FilePath: \\src\\index.ts
+ * @Description: 主文件入口
+ */
+
