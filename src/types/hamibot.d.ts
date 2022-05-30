c8501c899bc78b328d170545198a6b1791fc4bb7
BATU1579 <batu1234567890@outlook.com>
2022-05-30 16:57:29 +0800

    📃 docs: 补充文档

--- src/types/hamibot.d.ts
+++ src/types/hamibot.d.ts
@@ -2,7 +2,7 @@
  * @Author: BATU1579
  * @CreateDate: 2022-05-24 17:21:22
  * @LastEditor: BATU1579
- * @LastTime: 2022-05-30 14:34:35
+ * @LastTime: 2022-05-30 14:44:20
  * @FilePath: \\src\\types\\hamibot.d.ts
  * @Description: hamibot 模块
  */
@@ -114,12 +114,18 @@ declare enum MessageAttachmentType {
     image = "image"
 }
 
+/**
+ * @description: 运行在 Hamibot 的脚本，可以使用额外的环境信息和功能。
+ */
 declare namespace hamibot {
     /**
-     * @description: 脚本的环境变量
+     * @description: 包含环境信息和开发者自定义的脚本配置（参见 [在脚本中使用配置](https://docs.hamibot.com/tutorials/tutorial-config#%E5%9C%A8%E8%84%9A%E6%9C%AC%E4%B8%AD%E4%BD%BF%E7%94%A8%E9%85%8D%E7%BD%AE)）
      */
     let env: HamibotEnv;
 
+    /**
+     * @description: 脚本的定价计划信息，可用来区分版本。
+     */
     let plan: PricePlan;
 
     /**
