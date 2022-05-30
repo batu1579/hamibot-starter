dd961f218457cbcf974e0a4ed3339d1608040ea8
BATU1579 <batu1234567890@outlook.com>
2022-05-30 14:38:30 +0800

    🐞 fix: 修复不能获取配置项数据的问题
    现在可以正常使用 hamibot.env 获取配置项数据了，不过建议在使用之前检查数据

--- src/types/hamibot.d.ts
+++ src/types/hamibot.d.ts
@@ -2,18 +2,83 @@
  * @Author: BATU1579
  * @CreateDate: 2022-05-24 17:21:22
  * @LastEditor: BATU1579
- * @LastTime: 2022-05-25 18:43:33
+ * @LastTime: 2022-05-30 14:34:35
  * @FilePath: \\src\\types\\hamibot.d.ts
  * @Description: hamibot 模块
  */
 
-// TODO: 添加注释并补全类型
-interface MessageOptions {
-    telemetry: boolean;
-    data: MessageData
+interface HamibotEnv {
+    /**
+ * @description: 运行模式，可能是下列值之一：
+ *  - `development`
+ *  - `production`
+ */
+    APP_ENV: string;
+
+    /**
+     * @description: 用户 `id`
+     */
+    USER_ID: string;
+
+    /**
+     * @description: 机器人 `id`
+     */
+    ROBOT_ID: string;
+
+    /**
+     * @description: 脚本 `id`
+     */
+    SCRIPT_ID: string;
+
+    /**
+     * @description: 时间戳
+     * @example: "2021-02-03 04:05:06"
+     */
+    TIMESTAMP: string;
+
+    /**
+     * @description: 配置项的数据（建议使用前手动检查数据）
+     */
+    [prop: string]: any;
 }
 
-interface MessageData {
+/**
+ * @description: 脚本定价计划
+ */
+interface PricePlan {
+    /**
+     * @description: 计划名称（默认为 `免费` ）
+     */
+    name: string;
+    /**
+     * @description: 定价模式，可以是下列值之一：
+     *  - `free`
+     *  - `flatRate`
+     * （默认为 `free` ）
+     */
+    mode: string;
+    /**
+     * @description: 是否处于免费试用期（默认为 `false` ）
+     */
+    onFreeTrial: boolean;
+}
+
+/**
+ * @description: 消息选项
+ */
+declare interface MessageOptions {
+    telemetry: boolean;  // TODO： 询问这个键的意义
+
+    /**
+     * @description: 消息数据
+     */
+    data: MessageData;
+}
+
+/**
+ * @description: 消息数据（包括消息标题和消息附件）
+ */
+declare interface MessageData {
     /**
      * @description: 消息标题
      */
@@ -25,11 +90,14 @@ interface MessageData {
     attachments: MessageAttachment[];
 }
 
-interface MessageAttachment {
+/**
+ * @description: 消息附件
+ */
+declare interface MessageAttachment {
     /**
      * @description: 附件支持 text, json, image 三种类型，根据实际需要选择使用
      */
-    type: MessageAttachmentType,
+    type: MessageAttachmentType;
 
     /**
      * @description: 附件内容
@@ -37,6 +105,9 @@ interface MessageAttachment {
     data: string;
 }
 
+/**
+ * @description: 附件的可选类型
+ */
 declare enum MessageAttachmentType {
     text = "text",
     json = "json",
@@ -44,49 +115,12 @@ declare enum MessageAttachmentType {
 }
 
 declare namespace hamibot {
-    namespace env {
-        /**
-         * @description: 运行模式，可能是下列值之一：
-         *  - `development`
-         *  - `production`
-         */
-        let APP_ENV: string;
-        /**
-         * @description: 用户 `id`
-         */
-        let USER_ID: string;
-        /**
-         * @description: 机器人 `id`
-         */
-        let ROBOT_ID: string;
-        /**
-         * @description: 脚本 `id`
-         */
-        let SCRIPT_ID: string;
-        /**
-         * @description: 时间戳
-         * @example: "2021-02-03 04:05:06"
-         */
-        let TIMESTAMP: string;
-    }
+    /**
+     * @description: 脚本的环境变量
+     */
+    let env: HamibotEnv;
 
-    namespace plan {
-        /**
-         * @description: 计划名称（默认为 `免费` ）
-         */
-        let name: string;
-        /**
-         * @description: 定价模式，可以是下列值之一：
-         *  - `free`
-         *  - `flatRate`
-         * （默认为 `free` ）
-         */
-        let mode: string;
-        /**
-         * @description: 是否处于免费试用期（默认为 `false` ）
-         */
-        let onFreeTrial: boolean;
-    }
+    let plan: PricePlan;
 
     /**
      * @description: 将信息发送到控制台的脚本消息，实现远程查看。
@@ -119,10 +153,10 @@ declare namespace hamibot {
      * });
      * ```
      */
-    function postMessage(text: string, opts?: MessageOptions): void
+    function postMessage(text: string, opts?: MessageOptions): void;
 
     /**
      * @description: 确保 `hamibot.postMessage()` 发送成功后再退出。
      */
-    function exit(): void
+    function exit(): void;
 }
