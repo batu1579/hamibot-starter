72c6b08b25095e45571ee47b4e43e615433bef1f
BATU1579 <batu1234567890@outlook.com>
2022-09-23 19:38:15 +0800

    🐞 fix: 保证加载全局变量的顺序

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,11 @@
  * @Author: BATU1579
  * @CreateDate: 2022-05-24 16:58:03
  * @LastEditor: BATU1579
- * @LastTime: 2022-09-22 22:41:39
+ * @LastTime: 2022-09-23 17:45:32
  * @FilePath: \\src\\index.ts
  * @Description: 脚本入口
  */
-
+import {} from "./global";
 import { init } from "./lib/init";
 
-// 初始化
 init();
